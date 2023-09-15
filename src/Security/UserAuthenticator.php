aa24799a5c560f7ac7d50ba6ea5620453e470cba
Auxioma <auxioma@yandex.fr>
2023-09-15 12:56:47 +0200

    redirection login

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -48,7 +48,7 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        return new RedirectResponse($this->urlGenerator->generate('app_admin'));
     }
 
     protected function getLoginUrl(Request $request): string
